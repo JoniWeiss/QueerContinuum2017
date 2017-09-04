918749aff522c90b66b0821eca04cc1e7eef8142
Joni Weiss <me@joniweiss.com>
2017-09-04 10:03:09 -0600

    Recreated repository

---
+++ README.md
@@ -0,0 +1,30 @@
+# Queer Continuum Conference
+
+Created as a landing page and usable guide to an annual conference hosted by the Utah Pride Center, specifically for the 1to5 Club Utah, a program of the Utah Pride Center focussed on supporting the B+ Community (bisexual, asexual, pansexual, sexually fluid identities).
+
+## Built With
+
+* [AngularJS](https://github.com/angular/angular.js) - The web framework used
+* [UI-Router](https://github.com/angular-ui/ui-router) - Angular Routing
+* [MomentJS](https://momentjs.com/) - Used to manage date & time determinations.
+* [Angular Smooth Scroll](https://github.com/d-oliveros/ngSmoothScroll) - Angular library for scroll navigation.
+
+## Contributing
+
+* [Lea Verou (CSS Patterns Gallery)](http://lea.verou.me/css3patterns/) - Inspiration for CSS Radial Gradient pattern used as background.
+
+## Authors
+
+* **Joni Weiss** - *Initial work* - [Joni Weiss](https://github.com/JoniWeiss)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+### ReadMe format from Billie Thompson:
+* **Billie Thompson** - *Initial work* - [PurpleBooth](https://github.com/PurpleBooth)
+* Hat tip to anyone who's code was used
+* Inspiration
+* etc
---
+++ js/app.js
@@ -0,0 +1,210 @@
+/**
+ * Created by joniweiss on 7/24/15.
+ */
+var app = angular.module("QueerCon", ["ngAria", "ui.router", "smoothScroll"]);
+
+app.config([
+  "$stateProvider",
+  "$urlRouterProvider",
+  function($stateProvider, $urlRouterProvider) {
+    // // For any unmatched url, send to /route1
+    $urlRouterProvider.otherwise("/#");
+
+    $stateProvider
+      .state("home", {
+        url: "/",
+        templateUrl: "templates/home.html",
+        controller: "MainCtrl",
+        title: "Home"
+      })
+      .state("workshops", {
+        url: "/workshops",
+        templateUrl: "templates/workshops.html",
+        controller: "workshopsCtrl",
+        title: "Workshops"
+      })
+      .state("schedule", {
+        url: "/schedule",
+        templateUrl: "templates/schedule.html",
+        controller: "workshopsCtrl",
+        title: "Schedule"
+      })
+      .state("keynote", {
+        url: "/keynote",
+        templateUrl: "templates/keynote.html",
+        controller: "DefaultCtrl",
+        title: "Keynote"
+      })
+      .state("map", {
+        url: "/map",
+        templateUrl: "templates/map.html",
+        controller: "DefaultCtrl",
+        title: "Map"
+      })
+      .state("aboutUs", {
+        url: "/aboutUs",
+        templateUrl: "templates/about.html",
+        controller: "DefaultCtrl",
+        title: "About QueerCon"
+      })
+      .state("registerLink", {
+        url:
+          "https://utahpridecenter.secure.force.com/events/CnP_PaaS_EVT__ExternalRegistrationPage?event_id=a2G0a000000pgD5EAI",
+        external: true
+      });
+  }
+]);
+
+app.run(function($rootScope, $window) {
+  $rootScope.$on("$stateChangeStart", function(
+    event,
+    toState,
+    toParams,
+    fromState,
+    fromParams
+  ) {
+    if (toState.external) {
+      event.preventDefault();
+      $window.open(toState.url, "_blank");
+    }
+  });
+});
+
+app.directive("countdown", [
+  "Util",
+  "$interval",
+  function(Util, $interval) {
+    return {
+      restrict: "A",
+      scope: { date: "@" },
+      link: function(scope, element) {
+        var future;
+        future = new Date(scope.date);
+        $interval(function() {
+          var diff;
+          diff = Math.floor((future.getTime() - new Date().getTime()) / 1000);
+          // console.log(diff);
+          return element.text(Util.dhms(diff));
+        }, 1000);
+      }
+    };
+  }
+]);
+
+app.factory("Util", [
+  function() {
+    return {
+      dhms: function(t) {
+        var days, hours, minutes, seconds;
+        days = Math.floor(t / 86400);
+        t -= days * 86400;
+        hours = Math.floor(t / 3600) % 24;
+        t -= hours * 3600;
+        minutes = Math.floor(t / 60) % 60;
+        t -= minutes * 60;
+        seconds = t % 60;
+        return [days + "d", hours + "h", minutes + "m", seconds + "s"].join(
+          " "
+        );
+      }
+    };
+  }
+]);
+
+app.directive("backToTop", [
+  function() {
+    return {
+      restrict: "E",
+      transclude: true,
+      replace: true,
+      template: '<a href="#" class="back-to-top">Back to Top</a>',
+      link: function(scope, element) {
+        var amountScrolled = Math.round(screen.height * 0.5);
+
+        scope.button = element.find("button");
+
+        scope.button.on("click", function() {
+          document.body.animate({ scrollTop: 0 }, "fast");
+          element.removeClass("show");
+        });
+
+        window.addEventListener("scroll", function() {
+          if ($(window).scrollTop() > amountScrolled) {
+            $("a.back-to-top").fadeIn("slow");
+          } else {
+            $("a.back-to-top").fadeOut("slow");
+          }
+        });
+      }
+    };
+  }
+]);
+
+app.directive("copyrightString", function() {
+  return {
+    restrict: "E",
+    replace: true,
+    transclude: false,
+    template: "",
+    link: function(scope, element) {
+      var thisYear = new Date().getFullYear();
+      var openYear = 2015;
+      var companyName = "Utah Pride Center -- 1to5 Club, Salt Lake City, Utah";
+      var jcopyright = " Copyright " + openYear;
+      if (thisYear === openYear) {
+        jcopyright += ", " + companyName;
+      } else {
+        jcopyright += " - " + thisYear + ", " + companyName;
+      }
+      element.text(jcopyright);
+    }
+  };
+});
+
+app.controller("DefaultCtrl", [
+  "$scope",
+  "smoothScroll",
+  function($scope, smoothScroll) {
+    var element = document.getElementById("scrollToHere");
+    var options = {
+      duration: 700,
+      offset: 35,
+      easing: "easeInQuad"
+    };
+    smoothScroll(element, options);
+  }
+]);
+
+app.controller("workshopsCtrl", [
+  "$scope",
+  "$http",
+  "smoothScroll",
+  function($scope, $http, smoothScroll) {
+    $http.get("js/workshops.json").success(function(data) {
+      $scope.workshops = data;
+      $scope.oneAtATime = true;
+    });
+    var element = document.getElementById("scrollToHere");
+    var options = {
+      duration: 700,
+      offset: 35,
+      easing: "easeInQuad"
+    };
+    smoothScroll(element, options);
+  }
+]);
+
+app.controller("MainCtrl", [
+  "$scope",
+  "smoothScroll",
+  function($scope, smoothScroll) {
+    $scope.message = "Welcome to QueerCon!";
+    var element = document.getElementById("scrollToTop");
+    var options = {
+      duration: 700,
+      offset: 35,
+      easing: "easeInQuad"
+    };
+    smoothScroll(element, options);
+  }
+]);
